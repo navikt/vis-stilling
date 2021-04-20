d0dda6a2bff8571d16a200b3ca9a15b6cd14679c
Kjetil Svalestuen <kjesvale@users.noreply.github.com>
2021-04-20 10:16:43 +0200

    Ikke hent stillinger med ugyldig ID (#18)

--- src/mock/mock-api.ts
+++ src/mock/mock-api.ts
@@ -6,7 +6,7 @@ import upublisertStilling from './eksempler/upublisert-stilling.json';
 const basePath = '/arbeid/stilling';
 
 fetchMock
-    .get(`${basePath}/api/1`, stilling)
-    .get(`${basePath}/api/2`, annenStilling)
-    .get(`${basePath}/api/3`, upublisertStilling)
+    .get(`${basePath}/api/3446abca-2b01-4409-b2fb-0af0f668d204`, stilling)
+    .get(`${basePath}/api/821451e5-1515-4730-adcf-77632dff0e5e`, annenStilling)
+    .get(`${basePath}/api/a7ade3d9-4468-4cbc-8125-7903615f5e05`, upublisertStilling)
     .get(url => url.startsWith(`${basePath}/api`), annenStilling);
