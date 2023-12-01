c3298cb54d8f7f43ccf60eda1038f0e2861a34af
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2023-12-01 12:14:23 +0100

    Fiks mock-id og legg på stillingskategori i mock-data

--- src/mock/mock-api.ts
+++ src/mock/mock-api.ts
@@ -10,5 +10,5 @@ fetchMock
     .get(`${basePath}/api/3446abca-2b01-4409-b2fb-0af0f668d204`, stilling)
     .get(`${basePath}/api/821451e5-1515-4730-adcf-77632dff0e5e`, annenStilling)
     .get(`${basePath}/api/a7ade3d9-4468-4cbc-8125-7903615f5e05`, upublisertStilling)
-    .get(`${basePath}/api/2424ab59-23a0-1749-1756-1047ffff1098`, jobbmesse)
+    .get(`${basePath}/api/7b4d2a0c-8cc4-4c5b-b2a6-75992350d885`, jobbmesse)
     .get((url) => url.startsWith(`${basePath}/api`), annenStilling);
