b08f4e6809463141039d96b245d531e32efb6d0c
Martine Erdal Mansaker <martine.erdal.mansaker@nav.no>
2026-02-13 12:52:54 +0100

    Oppdater styling

--- app/_ui/Infopanel.tsx
+++ app/_ui/Infopanel.tsx
@@ -8,7 +8,7 @@ type InfopanelProps = {
 };
 
 const Infopanel: FunctionComponent<InfopanelProps> = ({ tittel, children, framhevet }) => {
-    const panelClass = framhevet ? 'rounded bg-[#cce1ff] p-5' : 'rounded bg-white p-5';
+    const panelClass = framhevet ? 'rounded bg-[#cce1ff] pb-10' : 'rounded bg-white pb-10';
 
     return (
         <div className={panelClass}>
