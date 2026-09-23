96d6ef475d3008a8a0b80bcf091bc05c17ffb1d2
haugse <sara.haugse@gmail.com>
2026-09-23 14:37:46 +0200

    Endre url-er

--- app/api/api-routes-with-obo.ts
+++ app/api/api-routes-with-obo.ts
@@ -8,7 +8,7 @@ export interface Iroute {
 }
 
 export const DelingAvCv: Iroute = {
-    api_route: '/rest/cv',
+    api_route: '/api',
     api_url: process.env.TOI_DELING_AV_CV_API ?? '',
     internUrl: '/arbeid/stilling/api/deling-av-cv',
     scope: `${miljø}:toi:toi-deling-av-cv-api`,
